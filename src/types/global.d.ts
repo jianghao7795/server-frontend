c663d0163c5977289e422da4693bab4a2ba9f0c3
jianghao <jianghao@haplox.com>
2024-04-25 17:53:16 +0800

    fix: 更改request请求

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,12 +1,12 @@
 declare namespace Global {
   export type Response<T> = {
-    code: 7 | 0;
+    code: 200 | 400 | 401 | 403 | 404 | 500;
     data: T;
     msg: string;
   };
 
   export type ResponseAbout<T> = {
-    code: 0 | 7;
+    code: 200 | 400 | 401 | 403 | 404 | 500;
     data?: T;
     msg: string;
   };
