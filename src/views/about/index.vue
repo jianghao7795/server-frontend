3d1fca03c2e46c58162f659aec0d285b9ea7eadc
jianghao <jianghao@haplox.com>
2024-10-08 16:17:15 +0800

    fix: 更新article typescript类型

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="view-content">
     <div v-if="userStore.currentUser?.user?.ID !== 0">
-      <h4>介绍: {{ userStore.currentUser.user.nickName }}</h4>
+      <h4>介绍: {{ userStore.currentUser?.user?.nickName }}</h4>
       <div class="about-content">
-        <Content :imgUrl="userStore.currentUser.user.introduction" />
+        <Content :imgUrl="userStore.currentUser?.user?.introduction" />
       </div>
     </div>
     <div v-else>请登录！</div>
