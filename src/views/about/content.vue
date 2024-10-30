d37070e41912e6c4e01dea64459415b0f2537a6d
jianghao <jianghao@haplox.com>
2024-10-30 18:25:12 +0800

    fix: update tsconfig

--- src/views/about/content.vue
+++ src/views/about/content.vue
@@ -2,12 +2,18 @@
   <p>{{ props.imgUrl }}</p>
 </template>
 
+<script lang="ts">
+export default {
+  name: "Content",
+};
+</script>
+
 <script lang="ts" setup name="Content">
-import { onMounted } from "vue";
+// import { onMounted } from "vue";
 
 const props = defineProps<{ imgUrl: string }>();
 
-onMounted(() => {
-  // console.log("234234234234");
-});
+// onMounted(() => {
+//   console.log("234234234234");
+// });
 </script>
