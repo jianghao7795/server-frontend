5bfbe7591ca7c3af31a87ad9dc2d3df28ea4c000
jianghao <978561120@qq.com>
2024-12-28 13:22:24 +0800

    fix: 修复typescript 的 declean

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,13 @@
-// /// <reference types="vite/client" />
+declare namespace Global {
+  export type Response<T> = {
+    code: 200 | 400 | 401 | 403 | 404 | 500;
+    data: T;
+    msg: string;
+  };
 
-// declare module "*.vue" {
-//   import { DefineComponent } from "vue";
-//   const component: DefineComponent<{}, {}, any>;
-//   export default component;
-// }
+  export type ResponseAbout<T> = {
+    code: 200 | 400 | 401 | 403 | 404 | 500;
+    data?: T;
+    msg: string;
+  };
+}
